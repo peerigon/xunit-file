772b121e93dca4702d74de0d34079bab26763dc8
Leonhard Melzer <leonhard.melzer@gmail.com>
2021-11-24 10:06:16 +0100

    fix: don't swallow error in test

--- test/output.spec.js
+++ test/output.spec.js
@@ -8,7 +8,10 @@ describe('output', function () {
 
   it('should write the report to the path specified by the XUNIT_FILE environment variable', function (done) {
     process.env.XUNIT_FILE = '_tmp/out/target.xml';
-    runFixture('success', function () {
+    runFixture('success', function (result) {
+      if (result.stderr) {
+        return done(new Error (result.stderr))
+      }
       var reportExists = fs.existsSync('_tmp/out/target.xml');
       (reportExists) ? done() : done(new Error('Report not found'));
     })
